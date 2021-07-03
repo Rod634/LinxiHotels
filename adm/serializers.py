2a164a1fc6a5d4525e1345f11301b07e2190296f
Rodrigo Silva <rodrigocsilva63@gmail.com>
2021-07-03 00:29:06 -0300

    serializing objects

--- adm/serializers.py
+++ adm/serializers.py
@@ -6,24 +6,24 @@ from django.contrib.auth.models import User
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Company
-        fields = ['name', 'adress', 'contactNumber', 'category', 'email']
+        fields = ['pk', 'name', 'adress', 'contactNumber', 'category', 'email']
 
 class ServiceSerializer(serializers.HyperlinkedModelSerializer):
     company = CompanySerializer(many=True)
     
     class Meta:
         model = Service
-        fields = ['name', 'price', 'peopleCount', 'period', 'description', 'company']
+        fields = ['pk', 'name', 'price', 'peopleCount', 'period', 'description', 'company']
 
 class RoomSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Room
-        fields = ['number', 'capacity', 'category', 'company', 'status']
+        fields = ['pk', 'number', 'capacity', 'category', 'company', 'status']
 
 class CustomerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Customer
-        fields = ['name', 'nationality', 'birth_date', 'address', 'contact_number', 'number_id', 'issue_id', 'passport', 'email']
+        fields = ['pk', 'name', 'nationality', 'birth_date', 'address', 'contact_number', 'number_id', 'issue_id', 'passport', 'email']
 
 class ReservationSerializer(serializers.HyperlinkedModelSerializer):
     customer = CustomerSerializer(many=False)
@@ -33,6 +33,6 @@ class ReservationSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Reservation
-        fields = ['customer', 'company', 'room', 'service', 'people_count', 'ocupation_date', 'leave_date', 'card_number', 'card_code']
+        fields = ['pk', 'customer', 'company', 'room', 'service', 'people_count', 'ocupation_date', 'leave_date', 'card_number', 'card_code']
 
 
